b6184a9359e4d334d09642478ce2ce5fbe0ed85d
unknown <saghar.fadaei@gmail.com>
2022-12-20 19:57:46 +0330

    remove extra environment variables

--- components/Common/utilities/envDecisionMaker.ts
+++
@@ -1,9 +0,0 @@
-
-export default function envDecisionMaker(page){
-    const mocked_pages = process.env.NEXT_PUBLIC_NOT_FINISHED_APIS.split(',');
-    if(mocked_pages.includes(page)){
-        return process.env.NEXT_PUBLIC_MOCK_HOST
-    }else {
-        return process.env.NEXT_PUBLIC_HOST
-    }
-}
